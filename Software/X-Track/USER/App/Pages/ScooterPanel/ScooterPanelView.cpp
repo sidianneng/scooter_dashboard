fbf1da865cc2a91920160dff744fea08f5cb7005
bbear2 <bbear_mail@163.com>
2021-12-18 16:55:22 +0800

    add the scooter panel app's main page for debug

--- Software/X-Track/USER/App/Pages/ScooterPanel/ScooterPanelView.cpp
+++ Software/X-Track/USER/App/Pages/ScooterPanel/ScooterPanelView.cpp
@@ -4,46 +4,107 @@
 
 using namespace Page;
 
-static void btn_event_cb(lv_event_t* e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t* btn = lv_event_get_target(e);
-    if (code == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
-
-        /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t* label = lv_obj_get_child(btn, 0);
-        lv_label_set_text_fmt(label, "Button: %d", cnt);
-    }
-}
-
 void ScooterPanelView::Create(lv_obj_t* root)
 {
-#if 0
-    lv_obj_t* label = lv_label_create(root);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 20);
-    lv_label_set_text(label, "");
-    ui.labelTitle = label;
+    PanelInfo_Create(root);
+    Button_Create(root);
+}
 
-    label = lv_label_create(root);
-    lv_label_set_text(label, "");
-    lv_obj_center(label);
-    ui.labelTick = label;
-#endif
-    lv_obj_t* btn = lv_btn_create(root);     /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-    lv_obj_set_size(btn, 220, 220);                          /*Set its size*/
-    //lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_PLUS, 0);
-    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+void ScooterPanelView::PanelInfo_Create(lv_obj_t* par)
+{
+    lv_obj_t* cont = lv_obj_create(par);
+    lv_obj_remove_style_all(cont);
+    lv_obj_set_size(cont, LV_HOR_RES, 180);
 
-    lv_obj_set_style_bg_opa(btn, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_color(btn, lv_color_hex(0xbbbbbb), LV_STATE_PRESSED);
-    lv_obj_set_style_bg_color(btn, lv_color_hex(0xff931e), LV_STATE_FOCUSED);
-    lv_obj_set_style_radius(btn, 19, 0);
+    lv_obj_set_style_bg_opa(cont, LV_OPA_COVER, 0);
+    lv_obj_set_style_bg_color(cont, lv_color_hex(0x333333), 0);
 
-    lv_obj_t* label = lv_label_create(btn);          /*Add a label to the button*/
-    lv_label_set_text(label, "Button");                     /*Set the labels text*/
-    lv_obj_center(label);
+    //speed number
+    lv_obj_t* label = lv_label_create(cont);
+    lv_obj_set_style_text_font(label, ResourcePool::GetFont("bahnschrift_65"), 0);
+    lv_obj_set_style_text_color(label, lv_color_white(), 0);
+    lv_label_set_text(label, "01");
+    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 53);
 
+    //battery capacity
+    lv_obj_t* label_capicity = lv_label_create(cont);
+    lv_obj_set_style_text_font(label_capicity, ResourcePool::GetFont("bahnschrift_32"), 0);
+    lv_obj_set_style_text_color(label_capicity, lv_color_white(), 0);
+    lv_label_set_text(label_capicity, "99");
+    lv_obj_align(label_capicity, LV_ALIGN_TOP_MID, 0, 133);
+
+    //bluetooth
+    lv_obj_t* label_ble = lv_label_create(cont);
+    lv_obj_set_style_text_font(label_ble, ResourcePool::GetFont("bahnschrift_32"), 0);
+    lv_obj_set_style_text_color(label_ble, lv_color_white(), 0);
+    lv_label_set_text(label_ble, "1");
+    lv_obj_align(label_ble, LV_ALIGN_TOP_MID, -100, 133);
+
+    //needfix
+    lv_obj_t* label_fix = lv_label_create(cont);
+    lv_obj_set_style_text_font(label_fix, ResourcePool::GetFont("bahnschrift_32"), 0);
+    lv_obj_set_style_text_color(label_fix, lv_color_white(), 0);
+    lv_label_set_text(label_fix, "2");
+    lv_obj_align(label_fix, LV_ALIGN_TOP_MID, -70, 53);
+
+    //temperate icon
+    lv_obj_t* label_temp = lv_label_create(cont);
+    lv_obj_set_style_text_font(label_temp, ResourcePool::GetFont("bahnschrift_32"), 0);
+    lv_obj_set_style_text_color(label_temp, lv_color_white(), 0);
+    lv_label_set_text(label_temp, "3");
+    lv_obj_align(label_temp, LV_ALIGN_TOP_MID, 70, 53);
+
+    //sport mode
+    lv_obj_t* label_mode = lv_label_create(cont);
+    lv_obj_set_style_text_font(label_mode, ResourcePool::GetFont("bahnschrift_32"), 0);
+    lv_obj_set_style_text_color(label_mode, lv_color_white(), 0);
+    lv_label_set_text(label_mode, "4");
+    lv_obj_align(label_mode, LV_ALIGN_TOP_MID, 100, 133);
+
+    ui.panelInfo.cont = cont;
+}
+
+void ScooterPanelView::Button_Create(lv_obj_t* par)
+{
+    lv_obj_t* cont = lv_obj_create(par);
+    lv_obj_remove_style_all(cont);
+    lv_obj_set_size(cont, LV_HOR_RES, 60);
+    lv_obj_align_to(cont, ui.panelInfo.cont, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
+
+    ui.btnCont.btnPower = Btn_Create(cont, ResourcePool::GetImage("start"), 0);
+}
+
+lv_obj_t* ScooterPanelView::Btn_Create(lv_obj_t* par, const void* img_src, lv_coord_t x_ofs)
+{
+    lv_obj_t* obj = lv_obj_create(par);
+    lv_obj_remove_style_all(obj);
+    lv_obj_set_size(obj, 240, 60);
+    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+
+    lv_obj_align(obj, LV_ALIGN_CENTER, x_ofs, 0);
+    lv_obj_set_style_bg_img_src(obj, img_src, 0);
+
+    lv_obj_set_style_bg_opa(obj, LV_OPA_COVER, 0);
+    lv_obj_set_style_width(obj, 240, LV_STATE_PRESSED);
+    lv_obj_set_style_height(obj, 60, LV_STATE_PRESSED);
+    lv_obj_set_style_bg_color(obj, lv_color_hex(0x666666), 0);
+    lv_obj_set_style_bg_color(obj, lv_color_hex(0xbbbbbb), LV_STATE_PRESSED);
+    lv_obj_set_style_bg_color(obj, lv_color_hex(0xff931e), LV_STATE_FOCUSED);
+
+    static lv_style_transition_dsc_t tran;
+    static const lv_style_prop_t prop[] = { LV_STYLE_WIDTH, LV_STYLE_HEIGHT, LV_STYLE_PROP_INV };
+    lv_style_transition_dsc_init(
+        &tran,
+        prop,
+        lv_anim_path_ease_out,
+        200,
+        0,
+        nullptr
+    );
+    lv_obj_set_style_transition(obj, &tran, LV_STATE_PRESSED);
+    lv_obj_set_style_transition(obj, &tran, LV_STATE_FOCUSED);
+
+    lv_obj_update_layout(obj);
+
+    return obj;
 }
--- Software/X-Track/USER/App/Pages/ScooterPanel/ScooterPanelView.h
+++ Software/X-Track/USER/App/Pages/ScooterPanel/ScooterPanelView.h
@@ -14,11 +14,31 @@ public:
 public:
     struct
     {
-        lv_obj_t* labelTitle;
-        lv_obj_t* labelTick;
+        struct
+        {
+            lv_obj_t* cont;
+            lv_obj_t* labelSpeed;
+            lv_obj_t* labelUint;
+
+            lv_obj_t* labelBluetooth;
+            lv_obj_t* labelWrench;
+            lv_obj_t* labelTemperature;
+            lv_obj_t* labelSportMode;
+
+            lv_obj_t* labelBatCapacity;
+        } panelInfo;
+
+        struct
+        {
+            lv_obj_t* btnPower;
+        } btnCont;
+
     } ui;
 
 private:
+    void PanelInfo_Create(lv_obj_t* par);
+    void Button_Create(lv_obj_t* par);
+    lv_obj_t* Btn_Create(lv_obj_t* par, const void* img_src, lv_coord_t x_ofs);
 
 };
 
